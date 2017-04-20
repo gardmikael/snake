4d1b3a8fa7edb51cb2817b4e1f8d8db2393d4187
Gard Mikael Fjeldavli <gardmikael@gmail.com>
2017-04-20 15:50:05 +0200

    Added asteroids and hit detection

--- ship.js
+++ ship.js
@@ -49,5 +49,9 @@ function Ship() {
 			this.pos.y = height + this.r;
 		}
 	}
+	this.hits = function(asteroid){
+		var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+		return d < this.r + asteroid.r;
+	}
 
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,37 @@
 var ship;
+var asteriods = [];
+var NUMBER_OF_ASTEROIDS = 8;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	ship = new Ship();
+	for (var i = 0; i < NUMBER_OF_ASTEROIDS; i++) {
+		asteriods.push(new Asteroid());	
+	}
+	
 }
 
 function draw() {
 	background(0);
+
+	for (var i = 0; i < asteriods.length; i++) {
+  		if(ship.hits(asteriods[i])){
+  			console.log("ship crashed");
+  			//code for crash ..end game
+  		}
+  		asteriods[i].render();
+  		asteriods[i].update();
+  		asteriods[i].edges();
+  	}
+
   	ship.render();
   	ship.turn();
   	ship.update();
   	ship.edges();
+
+
+  	
+
 }
 
 function keyPressed() {
